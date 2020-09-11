d7a97f79a33e37e348be048d381bc18482a7e1e8
menglingyu <menglingyu@xiaomi.com>
2020-09-11 16:44:56 +0800

    1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React from "react";
 import { Form } from "antd";
-// import Form, { Field } from "rc-field-form";
 
-// class Comp extends React.Component {
-//   componentDidMount() {
-//     console.log("did");
-//   }
-//   render() {
-//     return <div>comp</div>;
-//   }
-// }
+
 
 console.log(1);
-console.log(3);
-console.log(4);
+
+
 function App() {
   const [form] = Form.useForm();
   console.log(form, "internal");
